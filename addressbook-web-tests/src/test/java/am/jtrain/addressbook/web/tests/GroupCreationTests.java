62c5c17e918c4dc658c1636be4e3693043f51e4b
Magister_of_Hate <magisterofhate@gmail.com>
2022-07-29 23:32:46 +0300

    parametrized group creation test

--- addressbook-web-tests/src/test/java/am/jtrain/addressbook/web/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/am/jtrain/addressbook/web/tests/GroupCreationTests.java
@@ -2,7 +2,14 @@ package am.jtrain.addressbook.web.tests;
 
 import am.jtrain.addressbook.web.model.GroupData;
 import am.jtrain.addressbook.web.model.Groups;
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.*;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -10,18 +17,30 @@ import static org.testng.Assert.*;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> groupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/java/am/jtrain/addressbook/web/resources/groups.json"));
+        StringBuilder json = new StringBuilder();
+        String line = reader.readLine();
+        while (line != null) {
+            json.append(line);
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json.toString(), new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "groupsFromJson")
+    public void testGroupCreation(GroupData group) {
         app.navigate().groups();
         Groups before_list = app.group().readAll();
 
-        GroupData new_group = new GroupData().withName("group_1").withHeader("group_test 1").withFooter("group footer 1");
-
-        app.group().create(new_group);
+        app.group().create(group);
 
         assertEquals(app.group().count() , before_list.size() + 1);
         Groups after_list = app.group().readAll();
-        assertThat(after_list, equalTo(before_list.withAdded(new_group.withId(app.group().getMaxIdInList(app.group().getListIds())))));
+        assertThat(after_list, equalTo(before_list.withAdded(group.withId(app.group().getMaxIdInList(app.group().getListIds())))));
     }
 
 
