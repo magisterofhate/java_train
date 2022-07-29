c2eb0e54cad49fbdfd66719db8476bbba5958220
Magister_of_Hate <magisterofhate@gmail.com>
2022-07-29 23:35:56 +0300

    parametrized contact creation test

--- addressbook-web-tests/src/test/java/am/jtrain/addressbook/web/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/am/jtrain/addressbook/web/tests/ContactCreationTests.java
@@ -2,7 +2,18 @@ package am.jtrain.addressbook.web.tests;
 
 import am.jtrain.addressbook.web.model.ContactData;
 import am.jtrain.addressbook.web.model.Contacts;
+import am.jtrain.addressbook.web.model.GroupData;
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -10,20 +21,29 @@ import static org.testng.Assert.*;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> contactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/java/am/jtrain/addressbook/web/resources/contacts.json"));
+        StringBuilder json = new StringBuilder();
+        String line = reader.readLine();
+        while (line != null) {
+            json.append(line);
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json.toString(), new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "contactsFromJson")
+    public void testContactCreation(ContactData contact) {
         app.navigate().contacts();
         Contacts before_list = app.contact().readAll();
 
-        ContactData new_contact = new ContactData().withFirstname("Firstname").withLastname("Lastname").withMiddlename("Middlename")
-                .withAddress("address number one").withHome("+4456 7 89 123").withMobile("+33(789) 12-367")
-                .withPhone2("+55-14-289-73").withEmail("one.email@test.org").withEmail2("other.mail@test.school")
-                .withEmail3("asdrt@test.ru").withWork("+7(789)2471928");
-
-        app.contact().create(new_contact);
+        app.contact().create(contact);
         assertEquals(app.contact().count(), before_list.size() + 1);
         Contacts after_list = app.contact().readAll();
-        assertThat(after_list, equalTo(before_list.withAdded(new_contact.withId(app.contact().getMaxIdInList(app.contact().getListIds())))));
+        assertThat(after_list, equalTo(before_list.withAdded(contact.withId(app.contact().getMaxIdInList(app.contact().getListIds())))));
     }
 
 }
